d538c0d83c942ff41475438acc44db51b5e39b58
seanlin1125 <sean821125@gmail.com>
2022-09-06 14:44:21 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// require express-handlebars here
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect(process.env.MONGODB_RESTAURANT_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+const bodyParser = require('body-parser')
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
@@ -14,21 +17,42 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
 const Restaurant = require('./models/restaurant') // 載入 restaurant model
-// require express-handlebars here
-const exphbs = require('express-handlebars')
+
 // const restaurantList = require('./models/seeds/restaurant.json')
 // setting template engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // setting static files
 app.use(express.static('public'))
+// bodyParser
+app.use(bodyParser.urlencoded({ extended: true }))
 // routes setting
 app.get('/', (req, res) => {
   Restaurant.find() // 取出 Restaurant model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+// render new page
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+// Create data
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 // show page
 app.get('/restaurants/:restaurant_id', (req, res) => {
