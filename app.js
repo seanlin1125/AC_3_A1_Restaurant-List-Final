e6a4d961bab35b5dab0f9f1d148385414d6b8527
seanlin1125 <sean821125@gmail.com>
2022-09-10 11:57:13 +0800

    refactor: use req.body to simplify post and put code

--- app.js
+++ app.js
@@ -43,18 +43,12 @@ app.get('/', (req, res) => {
 app.get('/restaurants/new', (req, res) => {
   return res.render('new')
 })
+app.get('/restaurants/:id/restaurants/new', (req, res) => {
+  return res.render('new')
+})
 // Create data
 app.post('/restaurants', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })     // 存入資料庫
+  return Restaurant.create(req.body)     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
@@ -90,28 +84,7 @@ app.get('/restaurants/:id/edit', (req, res) => {
 // 修改資料
 app.put('/restaurants/:id', (req, res) => {
   const id = req.params.id
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
+  return Restaurant.findByIdAndUpdate(id, req.body)
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
 })
