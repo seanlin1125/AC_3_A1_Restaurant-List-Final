9d713fd3d06fed5611424d810962f71761e5808e
seanlin1125 <sean821125@gmail.com>
2022-11-02 11:16:23 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 // require packages used in the project
 const express = require('express')
-const app = express()
-const port = 3000
+const session = require('express-session')
 // require express-handlebars here
 const exphbs = require('express-handlebars')
-
 const bodyParser = require('body-parser')
-
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
 require('./config/mongoose')
+
+const app = express()
+const port = 3000
+
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 // setting static files
 app.use(express.static('public'))
 // bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
 
 app.use(routes)
-
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
