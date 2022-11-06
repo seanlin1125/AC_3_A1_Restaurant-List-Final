4f0df05b925aa35caa6e80d908f86864fce0a196
seanlin1125 <sean821125@gmail.com>
2022-11-06 11:37:36 +0800

    feat: modify user model

--- models/user.js
+++ models/user.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
